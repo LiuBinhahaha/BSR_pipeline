094775ea6a2cfe12c6dcc1a07917a05573d47bed
LiuBinhahaha <124219920+LiuBinhahaha@users.noreply.github.com>
2023-12-20 19:44:39 +0800

    Create 3.Separate_by_group.py

---
+++ No Parents/3.Separate_by_group.py
@@ -0,0 +1,16 @@
+# 按照技术路线将RNA-seqvcf文件拆分
+file = open("/data/heqiang/labmember/Zhengjun/BSR_RNA/RNA_seq.filter_ann.SNPs.vcf", 'r')
+out = open("/data/heqiang/labmember/Zhengjun/BSR_RNA/BES236_RNA.vcf", 'w')
+# out = open("/data/heqiang/labmember/Zhengjun/BSR_RNA/BES398_RNA.vcf", 'w')
+for line in file:
+    if line.startswith("#"):
+        if line.startswith("#CHROM"):
+            _line = line.rstrip().split("\t")
+            # out.write('\t'.join(_line[0:9]) + '\t' + '\t'.join(_line[11:13]) + '\n')  # BES398
+            out.write('\t'.join(_line[0:11]) + '\n')  # BES236
+    else:
+        _line = line.rstrip().split('\t')
+        # out.write('\t'.join(_line[0:9]) + '\t' + '\t'.join(_line[11:13]) + '\n')
+        out.write('\t'.join(_line[0:11]) + '\n')
+file.close()
+out.close()
